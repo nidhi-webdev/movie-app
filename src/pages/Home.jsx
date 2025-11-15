5dd9d60446eed825f71c1624f55d479439e3454c
nidhi <kumarinidhi0594@gmail.com>
2025-11-15 13:55:02 +0100

    Added the value and the onchange

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,31 @@
 import { nanoid } from 'nanoid';
 import MovieCard from '../components/movieCard';
+import { useState } from 'react';
 
 
 const Home = () => {
+    // For Search 
+    const [searchQuery, setSearchQuery] = useState("")
+
+
     const movie = [
         { id: nanoid(), title: "John Wick", release_date: 1999 },
         { id: nanoid(), title: "Terminator", release_date: 2000 },
         { id: nanoid(), title: "DDLJ", release_date: 1995 }
     ]
     const handleSubmit = () => {
-
     }
+
+
 
 
     return (
         <div className='home'>
             <form onSubmit={handleSubmit} className='search-form'>
-                <input type='text' placeholder='search For Movies...' className='search-input' />
+                <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}
+                type='text' 
+                placeholder='search For Movies...' 
+                className='search-input' />
                 <button type='submit' className='search-button'> Search </button>
             </form>
             <div className='movie-grid'>
