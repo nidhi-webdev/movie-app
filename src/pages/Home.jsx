521d5684666f797b8e7621fc4c4869574ec48326
nidhi <kumarinidhi0594@gmail.com>
2025-11-15 13:22:05 +0100

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,24 @@ import MovieCard from '../components/movieCard';
 
 const Home = () => {
     const movie = [
-        {id: nanoid(), title: "John Wick", release_date: 1999},
-        {id: nanoid(), title: "Terminator", release_date: 2000},
-        {id: nanoid(), title: "DDLJ", release_date: 1995}
+        { id: nanoid(), title: "John Wick", release_date: 1999 },
+        { id: nanoid(), title: "Terminator", release_date: 2000 },
+        { id: nanoid(), title: "DDLJ", release_date: 1995 }
     ]
 
-  return (
-    <div className='home'>
-      <div className='movie-grid'>
-       {movie.map(movie => <MovieCard movie={movie} key={movie.id} />)}
-      </div>
-    </div>
-  )
+   
+
+    return (
+        <div className='home'>
+            <form onSubmit={} className='search-form'>
+                <input type='text' placeholder='search For Movies...' className='search-input' />
+
+            </form>
+            <div className='movie-grid'>
+                {movie.map(movie => <MovieCard movie={movie} key={movie.id} />)}
+            </div>
+        </div>
+    )
 }
 
 export default Home
