0a64e94b4a536907bd2e74bb8f44060f6b393d66
nidhi <kumarinidhi0594@gmail.com>
2025-11-15 11:30:33 +0100

    Data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { nanoid } from 'nanoid';
+
 
 const Home = () => {
+    const movie = [
+        {id: nanoid(), title: "John Wick", release_date: 1999},
+        {id: nanoid(), title: "Terminator", release_date: 2000},
+        {id: nanoid(), title: "DDLJ", release_date: 1995}
+    ]
+
   return (
     <div>
       
