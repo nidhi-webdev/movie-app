35f8e20c52a1f75b884b4fe019b3db2c622e40e6
nidhi <kumarinidhi0594@gmail.com>
2025-11-17 11:44:10 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Fav from '../src/pages/fav'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
-  
+
 
   return (
+    <div>
+      <Navbar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/fav' element={<Fav />} />
+        </Routes>
 
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element = {<Home />}  />
-        <Route path='/fav' element= {<Fav />} />
-      </Routes>
+      </main>
+    </div>
 
-    </main>
-    
-  
-        
-    
+
+
+
+
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = () => {
     return (
         <nav className='navbar'>
             <div className='navbar-brand'>
-                <Link to= "/"> Movie App </Link>
+                <Link to="/"> Movie App </Link>
+            </div>
+            <div className='navbar-links'>
+                <Link to="/" className='nav-link'> Home </Link>
+                <Link to="/fav" className='nav-link'> Favorites </Link>
             </div>
         </nav>
     )
