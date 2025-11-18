87f7d76334f5431eb5d8690ec9e66a81af5ce1b7
nidhi <kumarinidhi0594@gmail.com>
2025-11-18 16:58:43 +0100

    Base_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,7 @@
 const API_KEY = "d6c68b28c783e06c555a04afeff05c6a"
+const BASE_URL = "https://www.themoviedb.org/"
+
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${BASE_UR}`)
+}
