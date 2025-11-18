fa2844443d868cd3a59b4ccf4d5ed3bc22c10b65
nidhi <kumarinidhi0594@gmail.com>
2025-11-18 17:03:31 +0100

    GetPopularMovie

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,6 @@ const BASE_URL = "https://www.themoviedb.org/"
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${BASE_UR}`)
+    const data = await response.json()
+    return data.results
 }
