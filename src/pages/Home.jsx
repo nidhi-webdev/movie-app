8a13360484ebff75407a0800b4ba42aff0710410
nidhi <kumarinidhi0594@gmail.com>
2025-11-19 17:13:25 +0100

    Useeffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,17 @@ const Home = () => {
     const [searchQuery, setSearchQuery] = useState("");
     const [movie, setMovie] = useState([]);
 
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            
+        }
+
+       
+    }, [])
 
 
-    
-      
+
+
     const handleSubmit = (e) => {
         e.preventDefault()
         if (searchQuery.trim()) {
@@ -31,15 +38,15 @@ const Home = () => {
                 {/* Search Form */}
                 <form onSubmit={handleSubmit} className='search-form max-w-2xl mx-auto mb-12'>
                     <div className='flex flex-col sm:flex-row gap-4 sm:gap-0'>
-                        <input 
-                            value={searchQuery} 
+                        <input
+                            value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
-                            type='text' 
-                            placeholder='Search for movies...' 
+                            type='text'
+                            placeholder='Search for movies...'
                             className='search-input flex-1 px-6 py-4 bg-white/10 backdrop-blur-md border border-white/20 rounded-l-xl sm:rounded-r-none rounded-r-xl text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-300'
                         />
-                        <button 
-                            type='submit' 
+                        <button
+                            type='submit'
                             className='search-button px-8 py-4 bg-linear-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 text-white font-semibold rounded-r-xl sm:rounded-l-none rounded-l-xl transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-purple-500'
                         >
                             Search
