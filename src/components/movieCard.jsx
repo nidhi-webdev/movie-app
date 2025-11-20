bed5ec021d91903fa83a96ed0bedb821f07c03ab
nidhi <kumarinidhi0594@gmail.com>
2025-11-20 17:10:14 +0100

    Getting the movie on the webpage

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -10,7 +10,7 @@ const MovieCard = ({ movie }) => {
     return (
         <div className='movie-card'>
             <div className='movie-poster'>
-                <img src={movie.url} alt={movie.title} />
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 <div className='movie-overlay'>
                     <button className='fav-btn' onClick={onFavClick}>
                         ♥
