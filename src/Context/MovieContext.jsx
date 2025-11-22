276145e6d835d250ac921ce9daaa141adb3d149d
nidhi <kumarinidhi0594@gmail.com>
2025-11-22 15:54:05 +0100

    getItem

--- src/Context/MovieContext.jsx
+++ src/Context/MovieContext.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { createContext } from "react";
 
 
@@ -6,6 +7,18 @@ const MovieContext = createContext()
 export const useMovieContext = useContext(MovieContext)
 
 export const MovieProvider = ({ children }) => {
+    const [favorites, setFavorites] = useState([])
+
+    useEffect(() => {
+      const storedFav = localStorage.getItem("favorites")
+
+      if(storedFav) setFavorites(JSON.parse(storedFav))
+    
+      return () => {
+        second
+      }
+    }, [third])
+    
 
 
     return <MovieContext.Provider>
