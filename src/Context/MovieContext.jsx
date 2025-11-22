22866dd131596be5af40ad1913098aabf8c23087
nidhi <kumarinidhi0594@gmail.com>
2025-11-22 16:13:41 +0100

    Function to remove from Fav

--- src/Context/MovieContext.jsx
+++ src/Context/MovieContext.jsx
@@ -26,6 +26,10 @@ export const MovieProvider = ({ children }) => {
         setFavorites(prev => [...prev, movie])
     }
 
+    const removeFromFavorites = (movieId) => {
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+    }
+
     return <MovieContext.Provider>
         {children}
     </MovieContext.Provider>
